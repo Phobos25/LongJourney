98f250d2db187d2f7e9789ab3277f193478f6558
Phobos25 <fydte@maill.ru>
2021-06-25 07:30:54 +0900

    update test programs

--- cpp/test_person_send.cpp
+++ cpp/test_person_send.cpp
@@ -94,137 +94,6 @@ private:
   int fail_count = 0;
 };
 
-class Person {
-public:
- void ChangeFirstName(int year, string first_name) {
-    // добавить факт изменения имени на first_name в год year
-    if (!IsChangedFirstName(year)){
-      data[year].firstname = first_name;
-      UpdateFirstName(year, first_name);
-    }
-  }
-
-  void ChangeLastName(int year, string last_name) {
-    // добавить факт изменения фамилии на last_name в год year
-    if(!IsChangedLastName(year)){
-      data[year].lastname = last_name;
-      UpdateLastName(year, last_name);
-    }
-  }
-
-  string GetFullName(int year) {
-    // получить имя и фамилию по состоянию на конец года year
-    sort(begin(years), end(years));
-    upper = upper_bound(begin(years), end(years), year);
-    int index = upper - years.begin() - 1 ;
-
-    if (index < 0){
-      index= 0;
-    }
-    if (years[index] > year){
-      return "Incognito";
-    }
-
-    int dyear = years[index];
-
-    if (data[dyear].firstname.length()>0 && data[dyear].lastname.length()>0){
-      return data[dyear].firstname + ' ' + data[dyear].lastname;
-    }
-    else if(data[dyear].firstname.length()>0 && data[dyear].lastname.length()==0){
-      return data[dyear].firstname + " with unknown last name";
-    }
-    else if(data[dyear].firstname.length()==0 && data[dyear].lastname.length()>0){
-      return data[dyear].lastname + " with unknown first name";
-    }
-    return "Incognito";
-  }
-private:
-  struct changed{
-    bool first = false;
-    bool last = false;
-  };
-  struct fullname{
-    string firstname;
-    string lastname;
-    changed is_changed;
-  };
-
-  vector<int>::iterator upper, lower;
-  vector<int> years;
-  map<int, fullname> data;
-
-  int GetIndex(const vector<int>& v, int K)
-  {
-      auto it = find(v.begin(), v.end(), K);
-
-      // If element was found
-      if (it != v.end())
-      {
-          // calculating the index
-          // of K
-          int index = it - v.begin();
-          return index;
-      }
-      else {
-          // If the element is not
-          // present in the vector
-          return -1;
-      }
-  }
-
-  bool IsChangedFirstName(const int& year){
-    if(data[year].is_changed.first == 0){return false;}
-    else {return true;}
-  }
-
-  bool IsChangedLastName(const int& year){
-    if(data[year].is_changed.last == 0){return false;}
-        else {return true;}
-  }
-
-  void UpdateFirstName(const int& year, const string& name){
-    years.push_back(year);
-    sort(begin(years), end(years));
-    int index = GetIndex(years, year);
-    for (int i=index; i<years.size(); ++i){
-      if (data[years[i]].firstname.length() != 0){
-        data[years[i]].firstname = name;
-      }
-    }
-    upper = upper_bound(begin(years), end(years), year);
-    index = upper - years.begin() - 1;
-    if (index < 0){
-      index = 0;
-    }
-    if (years[index] == year){
-      index-=1;
-    }
-    data[year].lastname = data[years[index]].lastname;
-    data[year].is_changed.first = true;
-  }
-
-  void UpdateLastName(const int& year, const string& name){
-    years.push_back(year);
-    sort(begin(years), end(years));
-    int index = GetIndex(years, year);
-    for (int i = index; i< years.size(); ++i){
-      if (data[years[i]].lastname.length() != 0){
-        data[years[i]].lastname = name;
-      }
-    }
-    upper = upper_bound(begin(years), end(years), year);
-    index = upper - years.begin() - 1;
-    if (index < 0){
-      index = 0;
-    }
-    if (years[index] == year){
-      index-=1;
-    }
-    data[year].firstname = data[years[index]].firstname;
-    data[year].is_changed.last = true;
-  }
-};
-
 void TestFirstName(){
     {
         Person person;
@@ -306,7 +175,7 @@ void TestLastName(){
     person.ChangeLastName(1983, "Yankovich");
     person.ChangeLastName(1975, "Yankovich");
     full_name = person.GetFullName(1977);
-    AssertEqual(full_name, "Watson with unknown first name");
+    AssertEqual(full_name, "Yankovich with unknown first name");
 
     full_name = person.GetFullName(1960);
     AssertEqual(full_name, "Incognito");
@@ -339,6 +208,7 @@ void TestFullName(){
     person.ChangeFirstName(1975, "Greg");    
     string full_name = person.GetFullName(1970);        
     AssertEqual(full_name, "House with unknown first name");
+    
     full_name = person.GetFullName(1971);
     AssertEqual(full_name, "Gregory House");
 
---
+++ cpp/test_rational.cpp
@@ -0,0 +1,143 @@
+#include <iostream>
+#include <map>
+#include <set>
+#include <sstream>
+#include <stdexcept>
+#include <string>
+#include <vector>
+#include <numeric>
+
+using namespace std;
+
+template <class T>
+ostream& operator << (ostream& os, const vector<T>& s) {
+  os << "{";
+  bool first = true;
+  for (const auto& x : s) {
+    if (!first) {
+      os << ", ";
+    }
+    first = false;
+    os << x;
+  }
+  return os << "}";
+}
+
+template <class T>
+ostream& operator << (ostream& os, const set<T>& s) {
+  os << "{";
+  bool first = true;
+  for (const auto& x : s) {
+    if (!first) {
+      os << ", ";
+    }
+    first = false;
+    os << x;
+  }
+  return os << "}";
+}
+
+template <class K, class V>
+ostream& operator << (ostream& os, const map<K, V>& m) {
+  os << "{";
+  bool first = true;
+  for (const auto& kv : m) {
+    if (!first) {
+      os << ", ";
+    }
+    first = false;
+    os << kv.first << ": " << kv.second;
+  }
+  return os << "}";
+}
+
+template<class T, class U>
+void AssertEqual(const T& t, const U& u, const string& hint = {}) {
+  if (t != u) {
+    ostringstream os;
+    os << "Assertion failed: " << t << " != " << u;
+    if (!hint.empty()) {
+       os << " hint: " << hint;
+    }
+    throw runtime_error(os.str());
+  }
+}
+
+void Assert(bool b, const string& hint) {
+  AssertEqual(b, true, hint);
+}
+
+class TestRunner {
+public:
+  template <class TestFunc>
+  void RunTest(TestFunc func, const string& test_name) {
+    try {
+      func();
+      cerr << test_name << " OK" << endl;
+    } catch (exception& e) {
+      ++fail_count;
+      cerr << test_name << " fail: " << e.what() << endl;
+    } catch (...) {
+      ++fail_count;
+      cerr << "Unknown exception caught" << endl;
+    }
+  }
+
+  ~TestRunner() {
+    if (fail_count > 0) {
+      cerr << fail_count << " unit tests failed. Terminate" << endl;
+      exit(1);
+    }
+  }
+
+private:
+  int fail_count = 0;
+};
+
+class Rational {
+  Rational(){
+    fraction = Fraction();
+  };
+  Rational(int numerator, int denominator){
+    if (denominator == 0){
+      throw invalid_argument("Invalid argument");
+    }
+    if (numerator*denominator >= 0){
+      fraction = Fraction(abs(numerator), abs(denominator)) ;
+    }else {
+      fraction = Fraction(-abs(numerator), abs(denominator)) ;
+    }
+
+  };
+
+  int Numerator() const{
+    return fraction.num;
+  };
+  int Denominator() const{
+    return fraction.denom;
+  };
+private:
+
+  struct Fraction{
+    int num;
+    int denom;
+    Fraction(int p=0, int q = 1){
+      int gcdenom = std::gcd(p, q);
+      num = p/gcdenom;
+      denom = q/gcdenom;
+    }
+  };
+  Fraction fraction;
+  int gcd1(int a, int b) {
+      if (b == 0)
+        return a;
+      return gcd1(b, a % b);
+    }
+
+};
+
+int main() {
+  TestRunner runner;
+  // добавьте сюда свои тесты
+  return 0;
+}
