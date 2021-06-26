1a55bfae0e29eeb645513350500b4a777a388ca6
PhobosHome <fydte@mail.ru>
2021-06-26 12:42:21 +0900

    refactoring

---
+++ cpp/TestSynonyms/test_synonyms.cpp
@@ -0,0 +1,42 @@
+#include "test_runner.h"
+#include <cassert>
+#include <exception>
+#include <vector>
+
+using namespace std;
+
+#include "synonyms.h"
+#include "tests.h"
+
+int main() {
+  TestAll();
+	int q ;
+	cin >> q;
+
+	map<string, set<string>> synonyms;
+
+	for (int i=0; i<q; ++i){
+		string operation_code;
+		cin >> operation_code;
+
+		if (operation_code == "ADD"){
+			string first_word, second_word;
+			cin >> first_word >> second_word;
+			AddSynonyms(synonyms, first_word, second_word);
+		}else if(operation_code == "COUNT"){
+			string word;
+			cin >> word;
+			cout << GetSynonymCount(synonyms, word) << endl;			
+		}else if (operation_code == "CHECK"){
+			string first_word, second_word;
+			cin >> first_word >> second_word;
+			if (AreSynonyms(synonyms, first_word, second_word)){
+				cout << "YES" << endl;
+			}else {
+				cout << "NO" << endl;
+			}
+		}
+	}
+
+	return 0;
+}
