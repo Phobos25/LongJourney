9a40908dc67ca73997e2b20d873212ce8be1424d
Phobos25 <fydte@maill.ru>
2021-06-26 19:58:38 +0900

    add sum_reverse_sort.cpp

--- cpp/sum_reverse/sum_reverse_sort.h
+++ cpp/sum_reverse/sum_reverse_sort.h
@@ -7,3 +7,12 @@ using namespace std;
 int Sum(int x, int y);
 string Reverse(string s);
 void Sort(vector<int>& nums);
+#pragma once
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int Sum(int x, int y);
+string Reverse(string s);
+void Sort(vector<int>& nums);
