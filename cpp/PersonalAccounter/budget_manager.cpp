ee19623bfcc1b02c2288395b0fbb807bf8a853ca
PhobosHome <fydte@mail.ru>
2021-07-03 13:44:39 +0900

    add budget_manager.cpp

--- cpp/PersonalAccounter/budget_manager.cpp
+++ cpp/PersonalAccounter/budget_manager.cpp
@@ -1,3 +1,5 @@
+#include <algorithm>
+#include <vector>
 #include "budget_manager.h"
 
 void BudgetManager::ComputeIncome(const Date& from, const Date& to)
@@ -8,5 +10,6 @@ void BudgetManager::ComputeIncome(const Date& from, const Date& to)
 void BudgetManager::Earn(const Date& from, const Date& to, 
                         const double& value)
 {
-    
+    std::vector <int> v;
+    std::partial_sum(v.begin(), v.end(),v.begin());
 }
