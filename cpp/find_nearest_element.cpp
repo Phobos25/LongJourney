5fc67dde2aed1b5818a71e04ea698c3156889588
Phobos25 <fydte@maill.ru>
2021-07-06 20:18:55 +0900

    add class_person_yellow

--- cpp/find_nearest_element.cpp
+++ cpp/find_nearest_element.cpp
@@ -6,48 +6,42 @@ using namespace std;
 set<int>::const_iterator FindNearestElement(
     const set<int>& numbers,
     int border){
-    
+
     if (numbers.empty()){
         return numbers.begin();
     }
     auto it = numbers.lower_bound(border);
-    if (border < *it){
-        int a = 2*border - *it;            
-        if (numbers.count(a)){
-            return numbers.find(a);
-        }
+    if ((it != numbers.begin()) & (it != numbers.end())){
+      if (border - *prev(it) <=  *it - border){
+        return prev(it);
+      }
+      else {
+        return it;
+      }
     }
+
     if (*prev(numbers.end()) < border){
         return prev(numbers.end());
     }
-    
+
     if (*numbers.begin() > border){
         return numbers.begin();
     }
     return it;
     }
 
-
-// set<int>::const_iterator —
-// тип итераторов для константного множес
-
 int main() {
-  set<int> numbers = {1, 4, 6};  
-//   *FindNearestElement(numbers, 0);
-//   *FindNearestElement(numbers, 3); 
-//   *FindNearestElement(numbers, 5); 
-//   *FindNearestElement(numbers, 6); 
-//   *FindNearestElement(numbers, 100);
 
+  set<int> numbers = {1, 4, 6};
   cout <<
       *FindNearestElement(numbers, 0) << " " <<
       *FindNearestElement(numbers, 3) << " " <<
       *FindNearestElement(numbers, 5) << " " <<
       *FindNearestElement(numbers, 6) << " " <<
       *FindNearestElement(numbers, 100) << endl;
-      
+
   set<int> empty_set;
-  
+
   cout << (FindNearestElement(empty_set, 8) == end(empty_set)) << endl;
   return 0;
 }
