c5dd6df5f8a25f529ea440053156aad217eebfb6
PhobosHome <fydte@mail.ru>
2021-07-07 13:59:42 +0900

    add stuff

--- cpp/FindStartsWith.cpp
+++ cpp/FindStartsWith.cpp
@@ -15,11 +15,21 @@ template <typename RandomIt>
 pair<RandomIt, RandomIt> FindStartsWith(
     RandomIt range_begin, RandomIt range_end,
     char prefix){
-       auto it =  binary_search(range_begin, range_end, prefix);
-       
+       auto lower = lower_bound(range_begin, range_end, prefix);
+       auto upper = upper_bound(range_begin, range_end, prefix);
+       return make_pair(lower, upper);
     }
 
-int main(){
 
-    return 0;
+int main() {
+const vector<string> sorted_strings = {"moscow", "murmansk", "vologda"};
+
+const auto m_result =
+    FindStartsWith(begin(sorted_strings), end(sorted_strings), 'm');
+for (auto it = m_result.first; it != m_result.second; ++it) {
+    cout << *it << " ";
+}
+cout << endl;
+
+return 0;
 }
