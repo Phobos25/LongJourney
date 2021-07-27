e7cc5be1b9b34c0d781b645ffbdb04a09905442c
PhobosHome <fydte@mail.ru>
2021-07-28 08:54:42 +0900

    white belt final 3

--- cpp/white_belt/White_final/project_template.cpp
+++ cpp/white_belt/White_final/project_template.cpp
@@ -12,17 +12,23 @@ using namespace std;
 // Реализуйте функции и методы классов и при необходимости добавьте свои
 
 class Date {
-public:
-  Date(){
-    year_ = 0;
-    month_ = 0;
-    day_ = 0;
-  }
+public: 
   Date(int year, int month, int day){
     year_ = year;
-    month_ = month;
-    day_ = day;
     
+    if (month < 1 || month > 12){                
+      string error_msg = "Month value is invalid: " + to_string(month);
+      throw runtime_error(error_msg);      
+    } 
+    
+    month_ = month;
+
+    if (day < 1 || day > 31){       
+        string error_msg = "Day value is invalid: " + to_string(day);
+        throw runtime_error(error_msg);        
+      } 
+    
+    day_ = day;    
   }
   int GetYear() const{
     return year_;
@@ -42,6 +48,7 @@ private:
 bool operator<(const Date& lhs, const Date& rhs);
 ostream& operator<<(ostream& stream, const Date& date);
 bool operator==(const Date& lhs, const Date& rhs);
+void CheckNextAndIgnore(istringstream& stream);
 
 Date ParseDate(istream& is);
 
@@ -52,37 +59,37 @@ public:
   }
 
   bool DeleteEvent(const Date& date, const string& event){    
-    if(db_[date].count(event) == 0){
-      return false; //такого ивента нте
-    } else {
+    if(db_.count(date) > 0 && db_[date].count(event) > 0){
       db_[date].erase(event);
       return true;
+    } else {
+      return false;
     }    
   }
 
   int DeleteDate(const Date& date){
+    if (db_.count(date) == 0){
+      return 0;
+    }    
     int number_of_events = db_[date].size();
-    db_[date].erase(db_[date].begin(), db_[date].end());
+    db_.erase(date);
     return number_of_events;
   }
 
-  void Find(const Date& date) const{
-    for (const auto& [key, value]: db_){
-      if (key == date){
-        for (const auto& v: value){
-          cout << v << "\n";
-        }
-      }      
+  set<string> Find(const Date& date) const{
+    if (db_.count(date) > 0){
+      return db_.at(date);
     }
+    return {};
   }
   
   void Print() const{
-    // private map <Date, Events> db_;
-    for (const auto& it:db_){
-      for (const auto& v:it.second){
-        cout << it.first << ' ' << v << "\n";
-      }
-  }
+    // private map <Date, Events> db_;    
+    for (const auto& [key, value]:db_){
+      for (const auto& v:value){
+        cout << key << ' ' << v << "\n";
+        }
+      }       
   }
 
 private:
@@ -110,75 +117,80 @@ bool operator==(const Date& lhs, const Date& rhs)
 	return lhs.GetYear() == rhs.GetYear() && lhs.GetMonth() == rhs.GetMonth() && lhs.GetDay() == rhs.GetDay();
 }
 
-Date ParseDate(string& date){
+void CheckNextAndIgnore(istringstream& stream, const string& date){
+  if (stream.peek() != '-'){    
+    string error_msg = "Wrong date format: " + date;
+    throw runtime_error(error_msg);
+  }
+  stream.ignore(1);
+}
+
+Date ParseDate(const string& date){
   istringstream is(date);
-  int year, month, day;
+  int year, month, day;    
+  
   is >> year;
-  is.ignore(1);
-  is >> month;
-  if (month <= 0 || month > 12){                
-      string error_msg = "Month value is invalid: " + to_string(month);
-      throw runtime_error(error_msg);      
-    } 
-  is.ignore(1);
+  CheckNextAndIgnore(is, date); 
+  is >> month;  
+  CheckNextAndIgnore(is, date);  
   is >> day;
-  if (day <= 0 || day > 31){       
-      string error_msg = "Day value is invalid: " + to_string(day);
-      throw runtime_error(error_msg);        
-    }      
+
+  if (!is.eof() || is.fail()){
+    string error_msg = "Wrong date format: " + date;
+    throw runtime_error(error_msg);      
+  }       
   return Date(year, month, day);
 }
 
-int main() {
-  Database db;
-  string command;
-  while (getline(cin, command)) {
+void InputHandler(const string& command, Database& db){
     string com, strDate, event;
     stringstream ss(command);
     ss >> com;
     // Считайте команды с потока ввода и обработайте каждую
     if (com == "Add"){      
-      ss >> strDate >> event;      
+      ss >> strDate >> event;            
       Date date = ParseDate(strDate);
       db.AddEvent(date, event);
     } else if (com == "Print"){
       db.Print();
     } else if (com == "Del"){
       ss >> strDate >> event;      
-      if (event.empty()){
-        try {
-          Date date = ParseDate(strDate);
-          int num = db.DeleteDate(date);
-          cout << "Deleted " << num << " events" << endl;
-        } catch (exception& ex){
-          cout << ex.what() << "\n";
-        }        
-      } else {
-        try {
-          Date date = ParseDate(strDate);
-          const bool b = db.DeleteEvent(date, event);
-          if (b){
-            cout << "Deleted successfully" << endl;
-          }else {
-            cout << "Event not found" << endl;
-          }} catch (exception& ex){
-          cout << ex.what() << "\n";  
-          }        
+      if (event.empty()){      
+        Date date = ParseDate(strDate);        
+        cout << "Deleted " << db.DeleteDate(date) << " events" << endl;      
+      } else {      
+        Date date = ParseDate(strDate);        
+        if (db.DeleteEvent(date, event)){
+          cout << "Deleted successfully" << endl;
+        }else {
+          cout << "Event not found" << endl;
+        }                
       }  
     } else if (com == "Find"){
-      ss >> strDate;
-      try{
-        Date date = ParseDate(strDate);
-        db.Find(date);
-      } catch (exception& ex){
-        cout << ex.what() << "\n";  
-      }        
+      ss >> strDate;    
+      Date date = ParseDate(strDate);
+      auto events = db.Find(date);         
+      if (!events.empty()){
+        for (auto& event:events){
+          cout << event << "\n";
+        }  
+      }    
     } else if (com.empty()){
-    } 
+       } 
      else{
-      cout << "Unknown command: "<< com << endl;
+      throw runtime_error("Unknown command: " + com );       
+    }
+}
+
+int main() {
+  Database db;
+  string command;
+  while (getline(cin, command)) {
+    try{
+      InputHandler(command, db);
+    } catch (exception& ex){
+      cout << ex.what() << "\n";  
     }
   }
-
   return 0;
 }
