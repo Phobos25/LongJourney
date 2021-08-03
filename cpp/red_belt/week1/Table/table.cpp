4cb3a8eade4a6d2d27d4bc46270bdda433d23d82
PhobosHome <fydte@mail.ru>
2021-08-03 19:59:55 +0900

    trying table cpp

--- cpp/red_belt/week1/Table/table.cpp
+++ cpp/red_belt/week1/Table/table.cpp
@@ -1,33 +1,60 @@
 #include "test_runner.h"
+#include <vector>
 
 using namespace std;
 
 // Реализуйте здесь шаблонный класс Table
 template <typename T>
 class Table{
-  Table(size_t rows, size_t cols);
+public:  
+  Table <T> (size_t rows, size_t cols){
+    rows_ = rows;
+    cols_ = cols;
+    v_.resize(rows);
+    for (size_t i=0; i<v_.size(); ++i){
+      v_.resize(cols);      
+    }
+  }
 
-  int& operator[](size_t rows, size_t cols);
-  int operator[](size_t rows, size_t cols) const;
+  int& operator[](size_t row, size_t col){
+    return v_[row][col];
+  }
+  int operator[](size_t row, size_t col) const{
+    return v_.at(row).at(col);
+  }
 
-  void Resize(size_t rows, size_t cols);
-  pair<size_t, size_t> Size() const;
+  void Resize(size_t rows, size_t cols){
+    for (size_t i=0; i<rows; ++i){
+      v_[i].resize(cols);
+    }
+    v_.resize(rows);
+  }
+
+  pair<size_t, size_t> Size() const{
+    return make_pair(rows_, cols_);
+  }
+
+private:
+  size_t rows_, cols_;
+  vector<vector<T>> v_;
 };
 
-
 void TestTable() {
-  Table<int> t(1, 1);
-  ASSERT_EQUAL(t.Size().first, 1u);
-  ASSERT_EQUAL(t.Size().second, 1u);
-  t[0][0] = 42;
-  ASSERT_EQUAL(t[0][0], 42);
-  t.Resize(3, 4);
-  ASSERT_EQUAL(t.Size().first, 3u);
-  ASSERT_EQUAL(t.Size().second, 4u);
+  // Table<int> t(1, 1);
+  // ASSERT_EQUAL(t.Size().first, 1u);
+  // ASSERT_EQUAL(t.Size().second, 1u);
+  // t[0][0] = 42;
+  // ASSERT_EQUAL(t[0][0], 42);
+  // t.Resize(3, 4);
+  // ASSERT_EQUAL(t.Size().first, 3u);
+  // ASSERT_EQUAL(t.Size().second, 4u);
 }
 
 int main() {
   TestRunner tr;
-  RUN_TEST(tr, TestTable);
+  // RUN_TEST(tr, TestTable);
+  Table<int> t(1, 1);
+  cout << t.Size().first << ' '
+       << t.Size().second << endl;
   return 0;
 }
--- cpp/test/test.cpp
+++ cpp/test/test.cpp
@@ -1,210 +1,27 @@
 #include <iostream>
-#include <string>
-#include <map>
-#include <set>
-#include <sstream>
-#include <iomanip>
+#include <vector>
 
 using namespace std;
 
-
-class Date {
-    friend istream &operator>>(istream &stream, Date &date) {
-        string DATE;
-        stream >> DATE;
-        int year, month, day;
-
-        stringstream ss(DATE);
-
-        ss >> year;
-        date.checkInput(ss, DATE);
-        date.SetYear(year);
-
-
-        ss >> month;
-        date.checkInput(ss, DATE);
-        date.SetMonth(month);
-
-        ss >> day;
-        if (ss.fail() || ss.peek() != -1) {
-            date.throwRuntimeException("Wrong date format: " + DATE);
-        }
-        date.SetDay(day);
-
-        return stream;
-    }
-
-    friend ostream &operator<<(ostream &stream, const Date &date) {
-        stream << setw(4) << setfill('0') << date.GetYear()
-               << "-"
-               << setw(2) << setfill('0') << date.GetMonth()
-               << "-"
-               << setw(2) << setfill('0') << date.GetDay();
-
-        return stream;
-    }
-
-public:
-    int GetYear() const {
-        return this->year;
-    };
-
-    int GetMonth() const {
-        return this->month;
-    };
-
-    int GetDay() const {
-        return this->day;
-    };
-
-    void SetYear(int year) {
-        this->year = year;
-    }
-
-    void SetMonth(int month) {
-        if (month > 0 && month < 13) {
-            this->month = month;
-        } else {
-            string error_msg = "Month value is invalid: " + to_string(month);
-            this->throwRuntimeException(error_msg);
-        }
-    }
-
-    void SetDay(int day) {
-        if (day > 0 && day < 32) {
-            this->day = day;
-        } else {
-            this->throwRuntimeException("Day value is invalid: " + to_string(day));
-        }
-    }
-
-private:
-    int year;
-    int month;
-    int day;
-
-    void checkInput(stringstream &stream, string &DATE) {
-        if (stream.fail() || stream.peek() != '-') {
-            this->throwRuntimeException("Wrong date format: " + DATE);
-        }
-
-        stream.ignore(1);
-    }
-
-    void throwRuntimeException(const string &error_msg) const {
-        throw runtime_error(error_msg);
-    }
-};
-
-bool operator<(const Date &lhs, const Date &rhs) {
-    return (lhs.GetYear() * 12 * 31 + lhs.GetMonth() * 31 + lhs.GetDay()) <
-           (rhs.GetYear() * 12 * 31 + rhs.GetMonth() * 31 + rhs.GetDay());
-};
-
-class Database {
-public:
-    void AddEvent(const Date &date, const string &event) {
-        this->data[date].insert(event);
-    };
-
-    bool DeleteEvent(const Date &date, const string &event) {
-        if (this->data.count(date) > 0) {
-            if (this->data[date].count(event) > 0) {
-                data[date].erase(event);
-                return true;
-            }
-        }
-        return false;
-    };
-
-    int DeleteDate(const Date &date) {
-        int number_of_events = data[date].size();
-        data.erase(date);
-        return number_of_events;
-    };
-
-    set<string> Find(const Date &date) const {
-        if (this->data.count(date) > 0) {
-            return this->data.at(date);
-        }
-        return {};
-    };
-
-    void Print() const {
-        if (this->data.size() > 0) {
-            for (const auto &record : this->data) {
-                for (const auto &event: record.second) {
-                    cout << record.first << " " << event << "\n";
-                }
-            }
-        } else {
-            cout << "" << "\n";
-        }
-    };
-
-private:
-    map<Date, set<string>> data;
-};
-
-void HandleInput(const string &command, Database &db) {
-
-    string op_code;
-    stringstream ss(command);
-    ss >> op_code;
-
-    if (op_code == "Add") {
-        Date date;
-        string event;
-        ss >> date >> event;
-        db.AddEvent(date, event);
-    } else if (op_code == "Del") {
-        Date date;
-        string event;
-        ss >> date >> event;
-        if (event.empty()) {
-            cout << "Deleted " << db.DeleteDate(date) << " events" << "\n";
-        } else {
-            if (db.DeleteEvent(date, event)) {
-                cout << "Deleted successfully" << "\n";
-            } else {
-                cout << "Event not found" << "\n";
-            }
-        }
-    } else if (op_code == "Find") {
-        Date date;
-        ss >> date;
-
-        auto events = db.Find(date);
-        if (!events.empty()) {
-            for (const auto &e: events) {
-                cout << e << "\n";
-            }
-        } else {
-            cout << "" << "\n";
-        }
-
-    } else if (op_code == "Print") {
-        db.Print();
-
-    } else if (op_code.empty()) {}
-    else {
-        throw runtime_error("Unknown command: " + op_code);
-    }
-
-
-}
-
 int main() {
-    Database db;
-    string command;
-    try {
-        while (getline(cin, command)) {
-            HandleInput(command, db);
+    vector<vector<int>> v = {{1,2}, 
+                             {3,4},
+                             {5,6}};
+    cout << "Original matrix: " << endl;
+    for (int i = 0; i< v.size(); ++i){
+        for (int j = 0; j<v[i].size(); ++j){
+            cout << v[i][j] << ' ';
         }
-
+        cout << endl;
     }
-    catch (exception &ex) {
-        cout << ex.what();
+    
+    v.resize(4);
+    cout << "After resize" << endl;
+    for (int i = 0; i< v.size(); ++i){
+        for (int j = 0; j<v[i].size(); ++j){
+            cout << v[i][j] << ' ';
+        }
+        cout << endl;
     }
     return 0;
 }
