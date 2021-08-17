c60fa9e552f8abedbac782a3b90160d002466dba
PhobosHome <fydte@mail.ru>
2021-08-17 21:38:09 +0900

    trying ebook 3

--- cpp/red_belt/week2/Ebook/main.cpp
+++ cpp/red_belt/week2/Ebook/main.cpp
@@ -1,3 +1,6 @@
+#include "test_runner.h"
+#include "profile.h"
+
 #include <iomanip>
 #include <iostream>
 #include <vector>
@@ -8,44 +11,26 @@ using namespace std;
 class ReadingManager {
 public:
   ReadingManager()
-      : user_page_counts_(MAX_USER_COUNT_ + 1, 0),
-        sorted_users_(),
-        user_positions_(MAX_USER_COUNT_ + 1, -1) {}
+      : users(MAX_USER_COUNT_ + 1, -1),        
+        pages(MAX_PAGE_COUNT_ + 1, 0) {}
 
   void Read(int user_id, int page_count) {
-    if (user_page_counts_[user_id] == 0) {
-      AddUser(user_id);
+    for (int i = users[user_id] + 1; i<=page_count; ++i){
+      ++pages[i];
     }
-    user_page_counts_[user_id] = page_count;
-    int& position = user_positions_[user_id];
-    while (position > 0 && page_count > user_page_counts_[sorted_users_[position - 1]]) {
-      SwapUsers(position, position - 1);
-    }
+    users[user_id] = page_count;
   }
 
-  double Cheer(int user_id) const {
-    if (user_page_counts_[user_id] == 0) {
+ double Cheer(int user_id) const {
+    if (users[user_id] == -1){
       return 0;
     }
-    const int user_count = GetUserCount();
-    if (user_count == 1) {
+    if (pages[0] == 1){
       return 1;
     }
-    const int page_count = user_page_counts_[user_id];
-    int position = user_positions_[user_id];
-    // тут можно реализовать бинарный поиск по массиву user_page_counts
-    while (position < user_count &&
-      user_page_counts_[sorted_users_[position]] == page_count) {
-      ++position;
-    }
-    if (position == user_count) {
-        return 0;
-    }
-    // По умолчанию деление целочисленное, поэтому
-    // нужно привести числитель к типу double.
-    // Простой способ сделать это — умножить его на 1.0.
-    return (user_count - position) * 1.0 / (user_count - 1);
-  }  
+
+    return (pages[0] - pages[users[user_id]]) * 1.0 / (pages[0]);
+  }
 private:
   // Статическое поле не принадлежит какому-то конкретному
   // объекту класса. По сути это глобальная переменная,
@@ -53,49 +38,173 @@ private:
   // Будь она публичной, к ней можно было бы обратиться снаружи
   // следующим образом: ReadingManager::MAX_USER_COUNT.
   static const int MAX_USER_COUNT_ = 100'000;
+  static const int MAX_PAGE_COUNT_ = 1'000;
 
-  vector<int> user_page_counts_;
-  vector<int> sorted_users_;   // отсортированы по убыванию количества страниц
-  vector<int> user_positions_; // позиции в векторе sorted_users_
-
-  int GetUserCount() const {
-    return sorted_users_.size();
-  }
-  void AddUser(int user_id) {
-    sorted_users_.push_back(user_id);
-    user_positions_[user_id] = sorted_users_.size() - 1;
-  }
-  void SwapUsers(int lhs_position, int rhs_position) {
-    const int lhs_id = sorted_users_[lhs_position];
-    const int rhs_id = sorted_users_[rhs_position];
-    swap(sorted_users_[lhs_position], sorted_users_[rhs_position]);
-    swap(user_positions_[lhs_id], user_positions_[rhs_id]);
-  }
-  int SearchPos(int lhs){
-    bool flag = false;
-    int rhs = sorted_users_[GetUserCount()-1];
-    int mid;
-    int key = user_page_counts_[lhs];
-    while ((lhs <= rhs) && ( flag != true)){
-      mid = (lhs + rhs) /2;
-      if (user_page_counts_[mid] < key ){
-        if (user_page_counts_[mid-1] == key){
-          flag = true;
-          --mid;
-        } else {
-          rhs = mid - 1;
-        }
-      }
-      if (user_page_counts_[mid] == key){
-        lhs = mid -1;
-      } else {
-        ++lhs;
-      }
-    }
-    return mid;
-  }
+  vector<int> users;
+  vector<int> pages;
+  
 };
 
+// ----------------------------------------------------------------------------------------------------
+void TestReadPage()
+{  LOG_DURATION("Total test duration") ;
+   {
+       ReadingManager manager;
+       ASSERT_EQUAL(manager.Cheer(1), 0.0);
+       manager.Read(1, 1);
+       ASSERT_EQUAL(manager.Cheer(1), 1.0);
+   }
+   {
+       ReadingManager manager;
+       ASSERT_EQUAL(manager.Cheer(1), 0.0);
+       manager.Read(1, 1);
+       ASSERT_EQUAL(manager.Cheer(2), 0.0);
+   }
+   {
+       ReadingManager manager;
+       manager.Read(1, 1);
+       ASSERT_EQUAL(manager.Cheer(1), 1.0);
+       manager.Read(1, 2);
+       ASSERT_EQUAL(manager.Cheer(1), 1.0);
+   }
+   {
+       ReadingManager manager;
+       manager.Read(1, 1);
+       manager.Read(2, 2);
+       manager.Read(3, 3);
+       ASSERT_EQUAL(manager.Cheer(2), 0.5);
+       manager.Read(4, 4);
+       ASSERT_EQUAL(manager.Cheer(2), 1.0 / 3.0);
+   }
+   {
+       ReadingManager manager;
+       manager.Read(1, 1);
+       manager.Read(2, 2);
+       manager.Read(3, 3);
+       ASSERT_EQUAL(manager.Cheer(2), 0.5);
+       manager.Read(4, 4);
+       ASSERT_EQUAL(manager.Cheer(3), 2.0 / 3.0);
+   }
+   {
+       ReadingManager manager;
+       manager.Read(1, 0);
+       manager.Read(2, 0);
+       manager.Read(3, 1);
+       manager.Read(4, 2);
+       ASSERT_EQUAL(manager.Cheer(3), 2.0 / 3.0);
+   }
+   {
+       ReadingManager manager;
+       manager.Read(1, 5);
+       manager.Read(2, 5);
+       manager.Read(3, 5);
+       manager.Read(4, 5);
+       ASSERT_EQUAL(manager.Cheer(3), 0.0);
+   }
+   {
+       ReadingManager manager;
+       manager.Read(4, 5);
+       manager.Read(3, 4);
+       manager.Read(2, 3);
+       manager.Read(1, 2);
+       ASSERT_EQUAL(manager.Cheer(3), 2.0 / 3.0);
+   }
+   {
+       ReadingManager manager;
+       manager.Read(4, 5);
+       manager.Read(3, 4);
+       manager.Read(4, 6);
+       manager.Read(3, 5);
+       ASSERT_EQUAL(manager.Cheer(4), 1.0);
+       ASSERT_EQUAL(manager.Cheer(3), 0.0);
+       manager.Read(10, 2);
+       ASSERT_EQUAL(manager.Cheer(3), 0.5);
+       ASSERT_EQUAL(manager.Cheer(4), 1.0);
+   }
+   {
+       ReadingManager manager;
+       manager.Read(4, 5);
+       ASSERT_EQUAL(manager.Cheer(4), 1.0);
+       manager.Read(4, 6);
+       ASSERT_EQUAL(manager.Cheer(4), 1.0);
+       manager.Read(4, 100);
+       ASSERT_EQUAL(manager.Cheer(4), 1.0);
+       ASSERT_EQUAL(manager.Cheer(3), 0.0);
+       ASSERT_EQUAL(manager.Cheer(5), 0.0);
+   }
+   {// ten users
+       ReadingManager manager;
+       manager.Read(1, 1);
+       manager.Read(2, 2);
+       manager.Read(3, 3);
+       manager.Read(4, 4);
+       manager.Read(5, 5);
+       manager.Read(6, 6);
+       manager.Read(7, 7);
+       manager.Read(8, 8);
+       manager.Read(9, 9);
+       manager.Read(100, 10);
+       manager.Read(111, 11);
+
+       ASSERT_EQUAL(manager.Cheer(1), 0.0);
+       ASSERT_EQUAL(manager.Cheer(2), 0.1);
+       ASSERT_EQUAL(manager.Cheer(3), 0.2);
+       ASSERT_EQUAL(manager.Cheer(4), 0.3);
+       ASSERT_EQUAL(manager.Cheer(5), 0.4);
+       ASSERT_EQUAL(manager.Cheer(6), 0.5);
+       ASSERT_EQUAL(manager.Cheer(7), 0.6);
+       ASSERT_EQUAL(manager.Cheer(8), 0.7);
+       ASSERT_EQUAL(manager.Cheer(9), 0.8);
+       ASSERT_EQUAL(manager.Cheer(100), 0.9);
+       ASSERT_EQUAL(manager.Cheer(111), 1.0);
+       ASSERT_EQUAL(manager.Cheer(12), 0.0);
+   }
+   {
+       ReadingManager manager;
+
+       ASSERT_EQUAL(manager.Cheer(1), 0.0);
+       ASSERT_EQUAL(manager.Cheer(1), 0.0);
+       manager.Read(1, 1);
+       ASSERT_EQUAL(manager.Cheer(1), 1.0);
+       ASSERT_EQUAL(manager.Cheer(1), 1.0);
+
+       ASSERT_EQUAL(manager.Cheer(2), 0);
+   }
+   {   LOG_DURATION("MAX_USER_COUNT1");
+       const int MAX_USER_COUNT = 100'000;
+       ReadingManager manager;
+       ASSERT_EQUAL(manager.Cheer(MAX_USER_COUNT), 0.0);
+       manager.Read(MAX_USER_COUNT, 1);
+       ASSERT_EQUAL(manager.Cheer(MAX_USER_COUNT), 1.0);
+   }
+   { 
+       ReadingManager manager;
+       ASSERT_EQUAL(manager.Cheer(1), 0.0);
+       manager.Read(1, 1000);
+       ASSERT_EQUAL(manager.Cheer(1), 1.0);
+   }
+   {   LOG_DURATION("MAX_USER_COUNT2");
+       const int MAX_USER_COUNT = 100'000;
+       ReadingManager manager;
+       ASSERT_EQUAL(manager.Cheer(MAX_USER_COUNT), 0.0);
+       manager.Read(MAX_USER_COUNT, 1000);
+       ASSERT_EQUAL(manager.Cheer(MAX_USER_COUNT), 1.0);
+   }
+   {
+       ReadingManager manager;
+       ASSERT_EQUAL(manager.Cheer(0), 0.0);
+       manager.Read(0, 1000);
+       ASSERT_EQUAL(manager.Cheer(0), 1.0);
+   }
+   {// from coursera (https://www.coursera.org/learn/c-plus-plus-red/programming/e72ST/eliektronnaia-knigha/discussions/threads/hLK3cT0AEemHtRKqiMW_lA)
+       ReadingManager manager;
+       manager.Read(1, 1);
+       manager.Read(1, 3);
+       manager.Read(2, 2);
+       ASSERT_EQUAL(manager.Cheer(1), 1.0);
+       ASSERT_EQUAL(manager.Cheer(2), 0.0);
+   }
+}
 
 int main() {
   // Для ускорения чтения данных отключается синхронизация
@@ -103,6 +212,9 @@ int main() {
   // а также выполняется отвязка cin от cout
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
+
+  TestRunner tr;
+  tr.RunTest(TestReadPage, "TestReadPage");
 
   ReadingManager manager;
 
--- cpp/test/test2.cpp
+++ cpp/test/test2.cpp
@@ -1,47 +1,33 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
+#include <map>
 
 using namespace std;
 
+struct Users{
+  int id;
+  int pages;  
+  Users (int i, int p){
+    id = i;
+    pages = p;
+  }
+};
+
+bool operator < (const Users& lhs, const Users& rhs){
+  return lhs.pages < rhs.pages;
+}
+
 int main() {
-  vector <int> v = {10, 8, 6, 6, 6, 6, 6, 6, 5, 4, 4,4,4,4,4, 2};
-  int key;
-  for (auto it:v){
-      cout << it << ' ';
-  }
-  cout << endl;
-  cout << "enter a value for search" << endl;
-  cin >> key;  
-//   sort(v.begin(), v.end());
-
-  bool flag = false;  
-  int l = 0;
-  int r = v.size()-1;
-  int mid;
-  cout << v.size() << endl;
-  while ((l <= r) && (flag != true)) {
-    mid = (l+r)/2;
-    if (v[mid] < key) { 
-        if (v[mid-1] == key){
-            flag = true;
-            mid--;
-        } else {
-            r = mid-1;
-        }
-    }
-    if (v[mid] == key) {
-        l = mid + 1;
-    } else {
-        l++;
-    }   
-  }
-
-  if (flag){
-        cout << "the element: "  << key 
-             << " was found at: "<< mid << endl;
-    } else {
-        cout << "the element couldn't be found" << endl;
-    }
+  vector<Users> users;
+  Users user1(1, 5);
+  Users user2(2, 3);
+  users.push_back(user1);
+  users.push_back(user2);
+  users.push_back(Users(3, 7));
+  users.push_back(Users(4, 10));
+  users.push_back(Users(5, 1));  
+  
+  
   return 0;
 }
