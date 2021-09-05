c0dcd07157cd5bed24da7733d6af9afa3e4ef075
Phobos25 <fydte@maill.ru>
2021-09-05 14:31:15 +0900

    add airport_counter.cpp

--- cpp/red_belt/week4/AirportCounter/airport_counter.cpp
+++ cpp/red_belt/week4/AirportCounter/airport_counter.cpp
@@ -53,41 +53,44 @@ template <typename TIterator>
 AirportCounter<TAirport>::AirportCounter(TIterator begin, TIterator end){
   auto it = begin;
   while (it != end){
-    data_[*it]++;
+    data_[static_cast<uint64_t>(*it)]++;
     ++it;
   }
 }
 
 template <typename TAirport>
 size_t AirportCounter<TAirport>:: Get(TAirport airport) const{
-  return data_[airport];
+  auto it = static_cast<uint64_t>(airport);
+  return data_[it];
 }
 
 template <typename TAirport>
 void AirportCounter<TAirport>:: Insert(TAirport airport){
-  data_[airport]++;
+  auto it = static_cast<uint64_t>(airport);
+  data_[it]++;
 }
 
 template <typename TAirport>
 void AirportCounter<TAirport>:: EraseOne(TAirport airport){
-  if (data_.at(airport) > 0){
-    data_[airport]--;
+  auto it = static_cast<uint64_t>(airport);
+  if (data_.at(it) > 0){
+    data_[it]--;
   }
 }
 
 template <typename TAirport>
 void AirportCounter<TAirport>:: EraseAll(TAirport airport){
-  data_[airport] = 0;
+  auto it = static_cast<uint64_t>(airport);
+  data_[it] = 0;
 }
 
 template <typename TAirport>
-Items AirportCounter<TAirport>:: GetItems() const{
-//  using Item = pair<TAirport, size_t>;
-//  using Items = array<Item,static_cast<uint64_t>(TAirport::Last_)>;
+array<pair<TAirport, size_t>,static_cast<uint64_t>(TAirport::Last_)> AirportCounter<TAirport>:: GetItems() const{
   Items item_array;
-  for (int i=0; i<size_; ++i){
-    item_array[i] = make_pair();
+  for (size_t i=0; i<size_; ++i){
+    item_array[i] = make_pair(static_cast<TAirport>(i), data_[i]);
   }
+  return item_array;
 }
 
 
