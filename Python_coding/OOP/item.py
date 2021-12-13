aebaff97650d17cb8dda0385b12c0fbc3dcb9bad
Phobos25 <fydte@maill.ru>
2021-12-13 19:04:13 +0300

    щщз знерщт

---
+++ Python_coding/OOP/item.py
@@ -0,0 +1,56 @@
+import csv
+
+class Item:
+    pay_rate = 0.8
+    all = []
+    # указываем тип переменной в конструкторе
+    # если есть переменная по умолчанию, то указывать не надо. переменная
+    # по умолчанию показывает, что ожидается получить
+    def __init__(self, name: str, price: float, quantity = 0):
+        # validate values
+        assert price >= 0, f"Price {price} is not greater than zero"
+        assert quantity >= 0, f"Quantity {quantity} is not greater than zero"
+        
+        # Assign to self object
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+        
+        # Actions to execute
+        Item.all.append(self)
+        
+    def calculate_total_price(self):
+        return self.price * self.quantity;
+    
+    def apply_discount(self):
+        self.price = self.price * self.pay_rate
+    
+    # class method
+    @classmethod
+    def instantiate_from_csv(cls, filename):
+        with open(filename, 'r') as f:
+            reader = csv.DictReader(f)
+            items = list(reader)
+        
+        # print read items
+        for item in items:
+            Item(
+                name  = item.get('name'),
+                price = float(item.get('price')),
+                quantity = int(item.get('quantity'))
+            )
+    
+    @staticmethod
+    def is_integer(num):
+        # we will count out floats that are point zero
+        # i.e. 5.0, 10.0, 0.0 etc.
+        if isinstance(num, float):
+            # count out the floats that are point zero
+            return num.is_integer()
+        elif isinstance(num, int):
+            return True
+        else:
+            return False
+        
+    def __repr__(self):
+        return f"{self.__class__.__name__}('{self.name}', {self.price}, {self.quantity})"
---
+++ Python_coding/OOP/phone.py
@@ -0,0 +1,17 @@
+from item import Item
+
+class Phone(Item):
+    def __init__(self, 
+                 name: str, 
+                 price: float, 
+                 quantity = 0, 
+                 broken_phones=0):
+        # call to super function to have access to all attributes / methods
+        super().__init__(
+            name, price, quantity
+        )    
+        # validate values        
+        assert broken_phones >= 0, f"Broken phones {broken_phones} is not greater than zero"
+        
+        # Assign to self object        
+        self.broken_phones = broken_phones
